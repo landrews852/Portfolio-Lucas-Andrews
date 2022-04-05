132136174ac0595182176cdcf357d0235a745746
landrews852 <85716642+landrews852@users.noreply.github.com>
2022-04-05 09:36:59 -0400

    finished

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import s from "./about.module.css";
-import picture from "../../images/lucas_andrews.png"
+import picture from "../../images/profile_picture.png"
 
 export default function About () {
   return (
@@ -21,22 +21,22 @@ export default function About () {
       <div className={s.skills}>
         <h3 className={s.skills_title}>Languages and Tools:</h3>
         <div className={s.skills_icons}>
-          <a href="https://nodejs.org" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/></a>
-          <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/></a>
-          <a href="https://www.docker.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="40" height="40"/></a>
-          <a href="https://expressjs.com" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/></a>
-          <a href="https://git-scm.com/" target="_blank" rel="noreferrer"><img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/></a>
-          <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/></a>
-          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/></a>
-          <a href="https://www.photoshop.com/en" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" width="40" height="40"/></a>
-          <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40"/></a>
-          <a href="https://postman.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40"/></a>
-          <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/></a>
-          <a href="https://redux.js.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="40" height="40"/></a>
-          <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/></a>
-          <a href="https://webpack.js.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/d00d0969292a6569d45b06d3f350f463a0107b0d/icons/webpack/webpack-original-wordmark.svg" alt="webpack" width="40" height="40"/> </a>
-          <a href="https://babeljs.io/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg" alt="babel" width="40" height="40"/> </a>
-          <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> </a>
+          <a className={s.link_skills} href="https://nodejs.org" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://www.docker.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://expressjs.com" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://git-scm.com/" target="_blank" rel="noreferrer"><img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://www.photoshop.com/en" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://postman.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://redux.js.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/></a>
+          <a className={s.link_skills} href="https://webpack.js.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/d00d0969292a6569d45b06d3f350f463a0107b0d/icons/webpack/webpack-original-wordmark.svg" alt="webpack" width="40" height="40"/> </a>
+          <a className={s.link_skills} href="https://babeljs.io/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg" alt="babel" width="40" height="40"/> </a>
+          <a className={s.link_skills} href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> </a>
         </div>
       </div>
     </section>
--- client/src/components/contact/contact.jsx
+++ client/src/components/contact/contact.jsx
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
+// import React, { useState } from 'react';
 import s from './contact.module.css';
-import Swal from 'sweetalert2'
+// import Swal from 'sweetalert2'
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Toaster, toast } from "react-hot-toast";
+
 // import { useForm } from 'react-hook-form';
 
 export default function Contact() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    Swal.fire({
-      title: 'Thank you!',
-      text: 'Your message has been sent!',
-      icon: 'success',
-      confirmButtonText: 'Cool'
-  })
-  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   Swal.fire({
+  //     title: 'Thank you!',
+  //     text: 'Your message has been sent!',
+  //     icon: 'success',
+  //     confirmButtonText: 'Cool'
+  // })
+  // }
 
-  const [error, setError] = useState("");
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: ""
-  })
+  // const [error, setError] = useState("");
+  // const [form, setForm] = useState({
+  //   name: "",
+  //   email: "",
+  //   message: ""
+  // })
 
   return (
     <section className={s.wrap}>
@@ -27,6 +31,42 @@ export default function Contact() {
         <h1>Contact</h1>
       </div>
       <div className={s.form}>
+        <div className={s.email}>
+          <CopyToClipboard text={"andrews852@gmail.com"}>
+            <a
+              className={s.link}
+              onClick={() => toast.success("Email copied to clipboard")}
+              >
+              <img
+                className={s.icon}
+                width="30px"
+                src="/images/email-icon.png"
+                />
+                <p className={s.txt}>andrews852@gmail.com</p>
+            </a>
+          </CopyToClipboard>
+          <Toaster position="bottom-center" reverseOrder={false} />
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/landrewsl" target="_blank" className={s.link}>
+            <img 
+              className={s.icon}  
+              src="/images/linkedin.png" 
+              />
+            <p className={s.txt}>linkedin.com/in/landrewsl</p>
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/landrews852" target="_blank" className={s.link}>
+            <img 
+              className={s.icon}  
+              src="/images/github.png" 
+              />
+            <p className={s.txt}>github.com/landrews852</p>
+          </a>
+        </div>
+      </div>
+      {/* <div className={s.form}>
         <form>
           <div className={s.form_input_wrap}>
             <label htmlFor="name">Name</label>
@@ -42,7 +82,7 @@ export default function Contact() {
           </div>
           <button className={s.btn} onClick={handleSubmit}>Submit</button>
         </form>
-      </div>
+      </div> */}
     </section>
   )
 }
